707613c111974aced700937a04c7685c411231cd
Štěpán Beneš <benes.stepan@gmail.com>
2025-06-11 12:08:22 +0200

    Serve frontend

--- weatherman/Program.cs
+++ weatherman/Program.cs
@@ -1,8 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 var summaries = new[]
 {
