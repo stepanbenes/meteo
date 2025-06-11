dc0da6c9c9c4fda027d8a3c7d345b854d5367f5a
Štěpán Beneš <benes.stepan@gmail.com>
2025-06-11 12:36:15 +0200

    Add some more scripts

---
+++ down.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker-compose down # To stop all containers started by docker-compose up
--- stop.sh
+++ stop.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker-compose down # To stop all containers started by docker-compose up:
-#docker-compose stop # To stop all containers without removing the network/volumes
+docker-compose stop # To stop all containers without removing the network/volumes
